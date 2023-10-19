4e89d685763153850624a3ca35f899e1b165e25a
F-VSSR <vssr@foxmail.com>
2023-10-19 21:09:10 +0800

    Change names

--- src/main/java/com/rhynia/astro/CommonProxy.java
+++ src/main/java/com/rhynia/astro/CommonProxy.java
@@ -1,4 +1,4 @@
-package com.myname.mymodid;
+package com.rhynia.astro;
 
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
--- src/main/java/com/rhynia/astro/Config.java
+++ src/main/java/com/rhynia/astro/Config.java
@@ -1,4 +1,4 @@
-package com.myname.mymodid;
+package com.rhynia.astro;
 
 import java.io.File;
 
