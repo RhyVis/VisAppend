943b82d843d5530a55b543537f68b50bc5aae664
F-VSSR <vssr@foxmail.com>
2023-10-20 00:10:49 +0800

    Fix

--- src/main/java/com/rhynia/astro/Astro.java
+++ src/main/java/com/rhynia/astro/Astro.java
@@ -1,45 +1,1 @@
-package com.rhynia.astro;
-
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
-import cpw.mods.fml.common.Mod;
-import cpw.mods.fml.common.SidedProxy;
-import cpw.mods.fml.common.event.FMLInitializationEvent;
-import cpw.mods.fml.common.event.FMLPostInitializationEvent;
-import cpw.mods.fml.common.event.FMLPreInitializationEvent;
-import cpw.mods.fml.common.event.FMLServerStartingEvent;
-
-@Mod(modid = Tags.MODID, version = Tags.VERSION, name = Tags.MODNAME, acceptedMinecraftVersions = "[1.7.10]")
-public class Astro {
-
-    public static final Logger LOG = LogManager.getLogger(Tags.MODID);
-
-    @SidedProxy(clientSide = "com.rhynia.astro.ClientProxy", serverSide = "com.rhynia.astro.CommonProxy")
-    public static CommonProxy proxy;
-
-    @Mod.EventHandler
-    // preInit "Run before anything else. Read your config, create blocks, items, etc, and register them with the
-    // GameRegistry." (Remove if not needed)
-    public void preInit(FMLPreInitializationEvent event) {
-        proxy.preInit(event);
-    }
-
-    @Mod.EventHandler
-    // load "Do your mod setup. Build whatever data structures you care about. Register recipes." (Remove if not needed)
-    public void init(FMLInitializationEvent event) {
-        proxy.init(event);
-    }
-
-    @Mod.EventHandler
-    // postInit "Handle interaction with other mods, complete your setup based on this." (Remove if not needed)
-    public void postInit(FMLPostInitializationEvent event) {
-        proxy.postInit(event);
-    }
-
-    @Mod.EventHandler
-    // register server commands in this event handler (Remove if not needed)
-    public void serverStarting(FMLServerStartingEvent event) {
-        proxy.serverStarting(event);
-    }
-}
+package com.rhynia.astro;import cpw.mods.fml.common.Mod;import cpw.mods.fml.common.SidedProxy;import cpw.mods.fml.common.event.FMLPreInitializationEvent;import org.apache.logging.log4j.LogManager;import org.apache.logging.log4j.Logger;@Mod(modid = Tags.MODID, version = Tags.VERSION, name = Tags.MODNAME, acceptedMinecraftVersions = "[1.7.10]")public class Astro {    public static final Logger LOG = LogManager.getLogger(Tags.MODID);    @SidedProxy(clientSide = "com.rhynia.astro.ClientProxy", serverSide = "com.rhynia.astro.CommonProxy")    public static CommonProxy proxy;    @Mod.EventHandler    public void preInit(FMLPreInitializationEvent event)    {        proxy.preInit(event);    }}
--- src/main/java/com/rhynia/astro/CommonProxy.java
+++ src/main/java/com/rhynia/astro/CommonProxy.java
@@ -1,30 +1,13 @@
 package com.rhynia.astro;
 
 import com.rhynia.astro.Material.MaterialAstra;
-import cpw.mods.fml.common.event.FMLInitializationEvent;
-import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
-import cpw.mods.fml.common.event.FMLServerStartingEvent;
 
 public class CommonProxy {
-
-    // preInit "Run before anything else. Read your config, create blocks, items, etc, and register them with the
-    // GameRegistry." (Remove if not needed)
     public void preInit(FMLPreInitializationEvent event) {
         Config.synchronizeConfiguration(event.getSuggestedConfigurationFile());
-
         Astro.LOG.info(Config.greeting);
         Astro.LOG.info("I am " + Tags.MODNAME + " at version " + Tags.VERSION);
-
         new MaterialAstra();
     }
-
-    // load "Do your mod setup. Build whatever data structures you care about. Register recipes." (Remove if not needed)
-    public void init(FMLInitializationEvent event) {}
-
-    // postInit "Handle interaction with other mods, complete your setup based on this." (Remove if not needed)
-    public void postInit(FMLPostInitializationEvent event) {}
-
-    // register server commands in this event handler (Remove if not needed)
-    public void serverStarting(FMLServerStartingEvent event) {}
 }
