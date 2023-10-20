47573cc5e01513c2ccd8a7116d45cc7d04417cbe
F-VSSR <vssr@foxmail.com>
2023-10-21 01:09:22 +0800

    Adding New

--- src/main/java/com/rhynia/astro/CommonProxy.java
+++ src/main/java/com/rhynia/astro/CommonProxy.java
@@ -8,9 +8,6 @@ import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 public class CommonProxy {
 
     public void preInit(FMLPreInitializationEvent event) {
-        Config.synchronizeConfiguration(event.getSuggestedConfigurationFile());
-        Astro.LOG.info(Config.greeting);
-        Astro.LOG.info("I am " + Tags.MODNAME + " at version " + Tags.VERSION);
         new MaterialAstra();
         new FluidReg();
     }
