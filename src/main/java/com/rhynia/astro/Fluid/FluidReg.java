40eaf63a5c4951bbb3181ced7b2efce345bf28d8
F-VSSR <vssr@foxmail.com>
2023-10-21 01:16:26 +0800

    Several Adds

--- src/main/java/com/rhynia/astro/Fluid/FluidReg.java
+++ src/main/java/com/rhynia/astro/Fluid/FluidReg.java
@@ -1,6 +1,6 @@
 package com.rhynia.astro.Fluid;
 
-import static gregtech.api.enums.FluidState.LIQUID;
+import static gregtech.api.enums.FluidState.*;
 
 import gregtech.api.fluid.GT_FluidFactory;
 
@@ -9,9 +9,25 @@ public class FluidReg implements Runnable {
     @Override
     public void run() {
         GT_FluidFactory.builder("AstroActivated")
-            .withLocalizedName("AstroActivated")
+            .withLocalizedName("激发态星辉超流体")
             .withStateAndTemperature(LIQUID, 250)
             .buildAndRegister()
             .asFluid();
+        GT_FluidFactory.builder("AstroUnstable")
+            .withLocalizedName("亚稳态星辉超流体")
+            .withStateAndTemperature(LIQUID, 25)
+            .buildAndRegister()
+            .asFluid();
+        GT_FluidFactory.builder("AstroLimited")
+            .withLocalizedName("极限态星辉超载体")
+            .withStateAndTemperature(LIQUID, 10)
+            .buildAndRegister()
+            .asFluid();
+        GT_FluidFactory.builder("AstroCatalyst")
+            .withLocalizedName("星辉催化载体")
+            .withStateAndTemperature(PLASMA, 1100)
+            .buildAndRegister()
+            .asFluid();
     }
+
 }
