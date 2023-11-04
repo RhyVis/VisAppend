b79fd7cb4f9f434eca507dac4a182ce027119512
RhyVis <vssr@foxmail.com>
2023-11-04 14:05:45 +0800

    Apply Spotless

--- src/main/java/com/rhynia/gtnhextend/GTNHExtend.java
+++ src/main/java/com/rhynia/gtnhextend/GTNHExtend.java
@@ -4,14 +4,9 @@ import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(
-    modid = Tags.MODID,
-    version = Tags.VERSION,
-    name = Tags.MODNAME,
-    acceptedMinecraftVersions = "[1.7.10]")
+@Mod(modid = Tags.MODID, version = Tags.VERSION, name = Tags.MODNAME, acceptedMinecraftVersions = "[1.7.10]")
 
 public class GTNHExtend {
-
 
     @SidedProxy(clientSide = "com.rhynia.gtnhextend.ClientProxy", serverSide = "com.rhynia.gtnhextend.CommonProxy")
     public static CommonProxy proxy;
