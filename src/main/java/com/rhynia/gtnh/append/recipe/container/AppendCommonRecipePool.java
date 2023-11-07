38cdf139e11aeb14874e3c8cacb6fd926054e8c1
RhyVis <vssr@foxmail.com>
2023-11-07 20:15:43 +0800

    Spotless

--- src/main/java/com/rhynia/gtnh/append/recipe/container/AppendCommonRecipePool.java
+++ src/main/java/com/rhynia/gtnh/append/recipe/container/AppendCommonRecipePool.java
@@ -1,19 +1,9 @@
 package com.rhynia.gtnh.append.recipe.container;
 
-import com.rhynia.gtnh.append.common.machine.recipe.GTAppendRecipe;
 import com.rhynia.gtnh.append.recipe.IRecipePool;
-import gregtech.api.enums.GT_Values;
-import gregtech.api.enums.ItemList;
-import gregtech.api.enums.Materials;
-import gregtech.api.util.GT_ModHandler;
-import gregtech.api.util.GT_Recipe;
-import gregtech.api.util.GT_Utility;
-
-import static gregtech.api.enums.Mods.GTPlusPlus;
-import static gregtech.api.enums.TierEU.RECIPE_UMV;
 
 public class AppendCommonRecipePool implements IRecipePool {
+
     @Override
-    public void loadRecipes() {
-    }
+    public void loadRecipes() {}
 }
