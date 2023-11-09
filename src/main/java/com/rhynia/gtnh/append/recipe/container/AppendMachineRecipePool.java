5a2650ddf8171dc4a829180b6c06d0c0dd46a647
RhyVis <vssr@foxmail.com>
2023-11-09 21:31:46 +0800

    Add machine recipe & update hatch

---
+++ src/main/java/com/rhynia/gtnh/append/recipe/container/AppendMachineRecipePool.java
@@ -0,0 +1,67 @@
+package com.rhynia.gtnh.append.recipe.container;
+
+import com.rhynia.gtnh.append.common.AppendItemList;
+import com.rhynia.gtnh.append.common.material.MaterialGTMethod;
+import com.rhynia.gtnh.append.loader.LoaderMachine;
+import com.rhynia.gtnh.append.recipe.IRecipePool;
+import gregtech.api.enums.*;
+import gregtech.api.util.GT_ModHandler;
+import gregtech.api.util.GT_OreDictUnificator;
+import gregtech.api.util.GT_Utility;
+
+import static gregtech.api.enums.TierEU.RECIPE_UEV;
+import static gregtech.api.enums.TierEU.RECIPE_UHV;
+import static gregtech.api.util.GT_RecipeBuilder.HOURS;
+import static gregtech.api.util.GT_RecipeConstants.*;
+
+public class AppendMachineRecipePool implements IRecipePool {
+    @Override
+    public void loadRecipes() {
+        // Infinite Liquid Air Hatch
+        GT_Values.RA.stdBuilder()
+            .metadata(RESEARCH_ITEM, GT_ModHandler.getModItem("gregtech", "gt.blockmachines", 1, 18999))
+            .metadata(RESEARCH_TIME, 2 * HOURS)
+            .itemInputs(
+                GT_ModHandler.getModItem("gregtech", "gt.blockmachines", 32, 18999),
+                ItemList.Electric_Pump_UEV.get(32),
+                ItemList.Field_Generator_UEV.get(12),
+                new Object[]{OrePrefixes.circuit.get(Materials.Infinite),8},
+                GT_ModHandler.getModItem("eternalsingularity", "eternal_singularity", 1),
+                Materials.Infinity.getPlates(4),
+                Materials.Infinity.getPlates(4),
+                Materials.Infinity.getPlates(4),
+                Materials.Infinity.getPlates(4))
+            .fluidInputs(
+                Materials.CosmicNeutronium.getFluid(10*144),
+                MaterialGTMethod.Astro.getFluid(16000),
+                MaterialGTMethod.AstroInf.getFluid(8000))
+            .itemOutputs(AppendItemList.AstraForge.get(1))
+            .noOptimize()
+            .eut(RECIPE_UEV)
+            .duration(140 * 20)
+            .addTo(AssemblyLine);
+
+        // Astra Forge
+        GT_Values.RA.stdBuilder()
+            .metadata(RESEARCH_ITEM, MaterialGTMethod.AstroInf.getGems(1))
+            .metadata(RESEARCH_TIME, 2 * HOURS)
+            .itemInputs(
+                ItemList.UHV_Coil.get(1),
+                ItemList.Field_Generator_UHV.get(8),
+                ItemList.Sensor_UHV.get(8),
+                Materials.Infinity.getPlates(12),
+                MaterialGTMethod.AstroMagic.getGems(16),
+                GT_OreDictUnificator.get(OrePrefixes.lens, MaterialGTMethod.AstroMagic, 32),
+                GT_OreDictUnificator.get(OrePrefixes.lens, MaterialGTMethod.AstroMagic, 32),
+                MaterialGTMethod.AstroMagic.getGems(16))
+            .fluidInputs(
+                Materials.CosmicNeutronium.getFluid(10*144),
+                MaterialGTMethod.Astro.getFluid(16000),
+                MaterialGTMethod.AstroInf.getFluid(8000))
+            .itemOutputs(AppendItemList.AstraForge.get(1))
+            .noOptimize()
+            .eut(RECIPE_UHV)
+            .duration(95 * 20)
+            .addTo(AssemblyLine);
+    }
+}
