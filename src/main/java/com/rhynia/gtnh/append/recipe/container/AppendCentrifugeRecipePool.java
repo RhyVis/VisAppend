737aef3a32473a4bea6964108b3aaac992722b82
Rhynia <vssr@foxmail.com>
2023-11-14 14:37:06 +0800

    Add Ti, W, Flu~ recipe

--- src/main/java/com/rhynia/gtnh/append/recipe/container/AppendCentrifugeRecipePool.java
+++ src/main/java/com/rhynia/gtnh/append/recipe/container/AppendCentrifugeRecipePool.java
@@ -1,7 +1,5 @@
 package com.rhynia.gtnh.append.recipe.container;
 
-import static gregtech.api.enums.TierEU.RECIPE_MV;
-
 import com.rhynia.gtnh.append.common.material.MaterialGTMethod;
 import com.rhynia.gtnh.append.recipe.IRecipePool;
 
@@ -9,12 +7,39 @@ import gregtech.api.enums.GT_Values;
 import gregtech.api.enums.Materials;
 import gregtech.api.util.GT_Recipe;
 
+import static gregtech.api.enums.TierEU.*;
+
 public class AppendCentrifugeRecipePool implements IRecipePool {
 
     @Override
     public void loadRecipes() {
         final GT_Recipe.GT_Recipe_Map CF = GT_Recipe.GT_Recipe_Map.sMultiblockCentrifugeRecipes;
 
+        // region 杂项系列
+        // 褐煤制煤
+        GT_Values.RA.stdBuilder()
+            .itemInputs(Materials.Lignite.getDust(4))
+            .itemOutputs(Materials.Coal.getDust(3))
+            .noOptimize()
+            .eut(RECIPE_LV)
+            .duration(8*20)
+            .addTo(CF);
+        // 氟碳铈
+        GT_Values.RA.stdBuilder()
+            .itemInputs(Materials.Bastnasite.getDust(12))
+            .itemOutputs(
+                Materials.Cerium.getDust(2),
+                Materials.Gadolinium.getDust(1),
+                Materials.Samarium.getDust(1),
+                Materials.Carbon.getDust(2))
+            .fluidOutputs(
+                Materials.Oxygen.getGas(6000),
+                Materials.Fluorine.getGas(2000)
+            )
+            .noOptimize()
+            .eut(RECIPE_HV)
+            .duration(8*20)
+            .addTo(CF);
         // 离心秘银 FePtMa=>CSMa+Pt+Ao
         GT_Values.RA.stdBuilder()
             .itemInputs(Materials.Mithril.getDust(6))
@@ -26,5 +51,6 @@ public class AppendCentrifugeRecipePool implements IRecipePool {
             .eut(RECIPE_MV)
             .duration(21 * 20)
             .addTo(CF);
+        // endregion
     }
 }
--- src/main/java/com/rhynia/gtnh/append/recipe/container/AppendElectroRecipePool.java
+++ src/main/java/com/rhynia/gtnh/append/recipe/container/AppendElectroRecipePool.java
@@ -4,16 +4,19 @@ import com.rhynia.gtnh.append.common.material.MaterialGTMethod;
 import com.rhynia.gtnh.append.recipe.IRecipePool;
 import gregtech.api.enums.GT_Values;
 import gregtech.api.enums.Materials;
+import gregtech.api.util.GT_ModHandler;
 import gregtech.api.util.GT_Recipe;
 
 import static gregtech.api.enums.TierEU.RECIPE_LV;
+import static gregtech.api.enums.TierEU.RECIPE_MV;
 
 public class AppendElectroRecipePool implements IRecipePool {
 
     @Override
     public void loadRecipes() {
-        final GT_Recipe.GT_Recipe_Map CF = GT_Recipe.GT_Recipe_Map.sMultiblockElectrolyzerRecipes;
+        final GT_Recipe.GT_Recipe_Map EC = GT_Recipe.GT_Recipe_Map.sMultiblockElectrolyzerRecipes;
 
+        // region 焙烧粉还原
         // 焙烧铁粉
         GT_Values.RA.stdBuilder()
             .itemInputs(Materials.RoastedIron.getDust(4))
@@ -23,7 +26,7 @@ public class AppendElectroRecipePool implements IRecipePool {
             .noOptimize()
             .eut(RECIPE_LV)
             .duration(8*20)
-            .addTo(CF);
+            .addTo(EC);
         // 焙烧铅粉
         GT_Values.RA.stdBuilder()
             .itemInputs(Materials.RoastedLead.getDust(4))
@@ -33,7 +36,7 @@ public class AppendElectroRecipePool implements IRecipePool {
             .noOptimize()
             .eut(RECIPE_LV)
             .duration(8*20)
-            .addTo(CF);
+            .addTo(EC);
         // 焙烧镍粉
         GT_Values.RA.stdBuilder()
             .itemInputs(Materials.RoastedNickel.getDust(4))
@@ -43,7 +46,7 @@ public class AppendElectroRecipePool implements IRecipePool {
             .noOptimize()
             .eut(RECIPE_LV)
             .duration(8*20)
-            .addTo(CF);
+            .addTo(EC);
         // 焙烧锌粉
         GT_Values.RA.stdBuilder()
             .itemInputs(Materials.RoastedZinc.getDust(4))
@@ -53,7 +56,7 @@ public class AppendElectroRecipePool implements IRecipePool {
             .noOptimize()
             .eut(RECIPE_LV)
             .duration(8*20)
-            .addTo(CF);
+            .addTo(EC);
         // 焙烧铜粉
         GT_Values.RA.stdBuilder()
             .itemInputs(Materials.RoastedCopper.getDust(4))
@@ -63,7 +66,7 @@ public class AppendElectroRecipePool implements IRecipePool {
             .noOptimize()
             .eut(RECIPE_LV)
             .duration(8*20)
-            .addTo(CF);
+            .addTo(EC);
         // 焙烧钴粉
         GT_Values.RA.stdBuilder()
             .itemInputs(Materials.RoastedCobalt.getDust(4))
@@ -73,7 +76,7 @@ public class AppendElectroRecipePool implements IRecipePool {
             .noOptimize()
             .eut(RECIPE_LV)
             .duration(8*20)
-            .addTo(CF);
+            .addTo(EC);
         // 焙烧锑粉
         GT_Values.RA.stdBuilder()
             .itemInputs(Materials.RoastedAntimony.getDust(4))
@@ -83,7 +86,7 @@ public class AppendElectroRecipePool implements IRecipePool {
             .noOptimize()
             .eut(RECIPE_LV)
             .duration(8*20)
-            .addTo(CF);
+            .addTo(EC);
         // 焙烧砷粉
         GT_Values.RA.stdBuilder()
             .itemInputs(Materials.RoastedArsenic.getDust(4))
@@ -93,6 +96,65 @@ public class AppendElectroRecipePool implements IRecipePool {
             .noOptimize()
             .eut(RECIPE_LV)
             .duration(8*20)
-            .addTo(CF);
+            .addTo(EC);
+        // endregion
+
+        // region 电解钨-钛系列
+        // 白钨
+        GT_Values.RA.stdBuilder()
+            .itemInputs(Materials.Scheelite.getDust(4))
+            .itemOutputs(
+                Materials.Tungsten.getDust(3),
+                Materials.Calcium.getDust(3))
+            .fluidOutputs(Materials.Oxygen.getGas(2000))
+            .noOptimize()
+            .eut(RECIPE_MV)
+            .duration(8*20)
+            .addTo(EC);
+        // 钨酸锂
+        GT_Values.RA.stdBuilder()
+            .itemInputs(GT_ModHandler.getModItem("bartworks", "gt.bwMetaGenerateddust", 6, 17))
+            .itemOutputs(
+                Materials.Tungsten.getDust(3),
+                Materials.Lithium.getDust(5))
+            .fluidOutputs(Materials.Oxygen.getGas(1500))
+            .noOptimize()
+            .eut(RECIPE_MV)
+            .duration(8*20)
+            .addTo(EC);
+        // 钨铁
+        GT_Values.RA.stdBuilder()
+            .itemInputs(GT_ModHandler.getModItem("bartworks", "gt.bwMetaGenerateddust", 6, 11))
+            .itemOutputs(
+                Materials.Tungsten.getDust(3),
+                Materials.Iron.getDust(2))
+            .fluidOutputs(Materials.Oxygen.getGas(1500))
+            .noOptimize()
+            .eut(RECIPE_MV)
+            .duration(8*20)
+            .addTo(EC);
+        // 钨酸锰
+        GT_Values.RA.stdBuilder()
+            .itemInputs(Materials.Tungstate.getDust(8))
+            .itemOutputs(
+                Materials.Tungsten.getDust(3),
+                Materials.Manganese.getDust(4))
+            .fluidOutputs(Materials.Oxygen.getGas(2000))
+            .noOptimize()
+            .eut(RECIPE_MV)
+            .duration(8*20)
+            .addTo(EC);
+        // 钛铁矿
+        GT_Values.RA.stdBuilder()
+            .itemInputs(Materials.Ilmenite.getDust(8))
+            .itemOutputs(
+                Materials.Titanium.getDust(5),
+                Materials.Iron.getDust(4))
+            .fluidOutputs(Materials.Oxygen.getGas(6000))
+            .noOptimize()
+            .eut(RECIPE_MV)
+            .duration(8*20)
+            .addTo(EC);
+        // endregion
     }
 }
