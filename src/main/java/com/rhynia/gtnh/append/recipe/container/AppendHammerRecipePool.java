699214615070c9c8c2e755dee04a9b0607d12d15
RhyVis <vssr@foxmail.com>
2023-11-15 19:05:31 +0800

    Moving comb recipes

--- src/main/java/com/rhynia/gtnh/append/recipe/container/AppendHammerRecipePool.java
+++ src/main/java/com/rhynia/gtnh/append/recipe/container/AppendHammerRecipePool.java
@@ -4,47 +4,56 @@ import com.rhynia.gtnh.append.common.material.MaterialGTMethod;
 import com.rhynia.gtnh.append.recipe.IRecipePool;
 import gregtech.api.enums.GT_Values;
 import gregtech.api.enums.Materials;
-import gregtech.api.util.GT_ModHandler;
 import gregtech.api.util.GT_Recipe;
-import gregtech.api.util.GT_Utility;
+import gregtech.common.items.CombType;
+import gregtech.loaders.misc.GT_Bees;
 import net.minecraftforge.fluids.FluidRegistry;
 import net.minecraftforge.fluids.FluidStack;
 
-import static gregtech.api.enums.TierEU.RECIPE_HV;
 import static gregtech.api.enums.TierEU.RECIPE_LuV;
 
 public class AppendHammerRecipePool implements IRecipePool {
 
     @Override
     public void loadRecipes() {
-        final GT_Recipe.GT_Recipe_Map LCR = GT_Recipe.GT_Recipe_Map.sMultiblockChemicalRecipes;
+        final GT_Recipe.GT_Recipe_Map HM = GT_Recipe.GT_Recipe_Map.sHammerRecipes;
 
         // region 杂项
-        // 磷化钙
+        // Am
         GT_Values.RA.stdBuilder()
             .itemInputs(
-                Materials.Calcium.getDust(3),
-                Materials.Phosphate.getDust(8),
-                GT_Utility.getIntegratedCircuit(2))
-            .itemOutputs(Materials.TricalciumPhosphate.getGems(5))
-            .noOptimize()
-            .eut(RECIPE_HV)
-            .duration(21 * 20)
-            .addTo(LCR);
-        // 干细胞
-        GT_Values.RA.stdBuilder()
-            .itemInputs(MaterialGTMethod.Astro.getDust(12), Materials.Osmiridium.getDust(8))
-            .itemOutputs(
-                GT_ModHandler.getModItem("gregtech", "gt.metaitem.03", 64, 32073),
-                GT_ModHandler.getModItem("gregtech", "gt.metaitem.03", 64, 32073),
-                GT_ModHandler.getModItem("gregtech", "gt.metaitem.03", 64, 32073),
-                Materials.MysteriousCrystal.getDust(2))
-            .fluidInputs(Materials.GrowthMediumSterilized.getFluid(4000))
-            .fluidOutputs(new FluidStack(FluidRegistry.getFluid("bacterialsludge"), 4000))
+                GT_Bees.combs.getStackForType(CombType.AMERICIUM,16),
+                MaterialGTMethod.Astro.getDust(4))
+            .itemOutputs(Materials.Americium.getDust(32))
+            .fluidOutputs(MaterialGTMethod.Astro.getFluid(125))
             .noOptimize()
             .eut(RECIPE_LuV)
-            .duration(21 * 20)
-            .addTo(LCR);
+            .duration(25 * 20)
+            .addTo(HM);
+        // Nt
+        GT_Values.RA.stdBuilder()
+            .itemInputs(
+                GT_Bees.combs.getStackForType(CombType.NEUTRONIUM,16),
+                MaterialGTMethod.Astro.getDust(12))
+            .itemOutputs(Materials.Neutronium.getIngots(2))
+            .fluidOutputs(MaterialGTMethod.Astro.getFluid(12),Materials.Neutronium.getMolten(2304))
+            .noOptimize()
+            .eut(RECIPE_LuV)
+            .duration(25 * 20)
+            .addTo(HM);
+        // Kevlar
+        GT_Values.RA.stdBuilder()
+            .itemInputs(
+                GT_Bees.combs.getStackForType(CombType.KEVLAR,16),
+                MaterialGTMethod.Astro.getDust(12))
+            .itemOutputs(Materials.Neutronium.getIngots(2))
+            .fluidOutputs(
+                MaterialGTMethod.Astro.getFluid(12),
+                new FluidStack(FluidRegistry.getFluid("molten.kevlar"),4608))
+            .noOptimize()
+            .eut(RECIPE_LuV)
+            .duration(25 * 20)
+            .addTo(HM);
         // endregion
     }
 }
