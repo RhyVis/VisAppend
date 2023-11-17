c8e0e54c2426aae34122327235498bdd97fb4f3e
RhyVis <vssr@foxmail.com>
2023-11-17 22:18:18 +0800

    Rebuild assets, add Ultimate.

--- src/main/java/com/rhynia/gtnh/append/common/item/ItemBasic.java
+++ src/main/java/com/rhynia/gtnh/append/common/item/ItemBasic.java
@@ -2,6 +2,7 @@ package com.rhynia.gtnh.append.common.item;
 
 import static com.rhynia.gtnh.append.client.GTNHATab.tabMetaItem01;
 
+import com.rhynia.gtnh.append.common.item.container.ItemUltimate;
 import net.minecraft.item.Item;
 
 import com.rhynia.gtnh.append.common.item.registry.RegItem;
@@ -10,5 +11,7 @@ public final class ItemBasic {
 
     public static final Item MetaItem01 = new RegItem("MetaItem01Base", "MetaItem01", tabMetaItem01)
         .setTextureName("append:MetaItem01/0");
+
+    public static final Item Ultimate = new ItemUltimate(tabMetaItem01).setTextureName("append:ultimate");
 
 }
--- src/main/java/com/rhynia/gtnh/append/common/item/ItemRegister.java
+++ src/main/java/com/rhynia/gtnh/append/common/item/ItemRegister.java
@@ -2,14 +2,16 @@ package com.rhynia.gtnh.append.common.item;
 
 import static com.rhynia.gtnh.append.common.item.ItemBasic.MetaItem01;
 
+import com.rhynia.gtnh.append.common.AppendItemList;
 import net.minecraft.item.Item;
 
 import cpw.mods.fml.common.registry.GameRegistry;
+import net.minecraft.item.ItemStack;
 
 public class ItemRegister {
 
     public static void registryItems() {
-        Item[] itemsToReg = { MetaItem01 };
+        Item[] itemsToReg = { ItemBasic.Ultimate };
 
         for (Item item : itemsToReg) {
             GameRegistry.registerItem(item, item.getUnlocalizedName());
@@ -18,7 +20,9 @@ public class ItemRegister {
     }
 
     // spotless:off
-    public static void registryItemContainers() {}
+    public static void registryItemContainers() {
+        AppendItemList.ItemUltimate.set(new ItemStack(ItemBasic.Ultimate,1));
+    }
     // spotless:on
     public static void registry() {
         registryItems();
