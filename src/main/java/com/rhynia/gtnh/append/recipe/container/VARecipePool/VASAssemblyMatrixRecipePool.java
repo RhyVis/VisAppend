96e5bd27a7018b97e9ea81ffb42b99210184956a
RhyVis <vssr@foxmail.com>
2023-11-20 21:47:27 +0800

    Tweak test recipe

--- src/main/java/com/rhynia/gtnh/append/recipe/container/VARecipePool/VASAssemblyMatrixRecipePool.java
+++ src/main/java/com/rhynia/gtnh/append/recipe/container/VARecipePool/VASAssemblyMatrixRecipePool.java
@@ -2,6 +2,7 @@ package com.rhynia.gtnh.append.recipe.container.VARecipePool;
 
 import static gregtech.api.enums.TierEU.*;
 
+import com.rhynia.gtnh.append.common.VAItemList;
 import com.rhynia.gtnh.append.common.machine.mapRecipe.VARecipe;
 import com.rhynia.gtnh.append.common.material.VA_GregtechMaterialPool;
 import com.rhynia.gtnh.append.common.material.VA_WerkstoffMaterialPool;
@@ -9,6 +10,7 @@ import com.rhynia.gtnh.append.recipe.IRecipePool;
 
 import gregtech.api.enums.GT_Values;
 import gregtech.api.enums.Materials;
+import gregtech.api.enums.MaterialsUEVplus;
 import gregtech.api.enums.OrePrefixes;
 import gregtech.api.util.GT_OreDictUnificator;
 import gregtech.api.util.GT_Recipe;
@@ -21,29 +23,29 @@ public class VASAssemblyMatrixRecipePool implements IRecipePool {
         // 透镜配方
         GT_Values.RA.stdBuilder()
             .itemInputs(
-                GT_OreDictUnificator.get(OrePrefixes.lens, VA_GregtechMaterialPool.AstroMagic, 4),
-                GT_OreDictUnificator.get(OrePrefixes.lens, VA_GregtechMaterialPool.AstroMagic, 4),
-                VA_WerkstoffMaterialPool.Primogem.get(OrePrefixes.lens,64),
-                VA_WerkstoffMaterialPool.Originiums.get(OrePrefixes.lens,64),
-                GT_OreDictUnificator.get(OrePrefixes.lens, VA_GregtechMaterialPool.AstroMagic, 4),
-                GT_OreDictUnificator.get(OrePrefixes.lens, VA_GregtechMaterialPool.AstroMagic, 4),
+                GT_OreDictUnificator.get(OrePrefixes.lens, VA_GregtechMaterialPool.AstroInf, 64),
+                GT_OreDictUnificator.get(OrePrefixes.lens, VA_GregtechMaterialPool.AstroInf, 64),
+                VA_WerkstoffMaterialPool.Primogem.get(OrePrefixes.lens, 64),
+                VA_WerkstoffMaterialPool.Originiums.get(OrePrefixes.lens, 64),
+                GT_OreDictUnificator.get(OrePrefixes.lens, VA_GregtechMaterialPool.AstroMagic, 64),
+                GT_OreDictUnificator.get(OrePrefixes.lens, VA_GregtechMaterialPool.AstroMagic, 64),
                 VA_GregtechMaterialPool.Astro.getDust(64),
                 VA_GregtechMaterialPool.Astro.getDust(64),
-                VA_GregtechMaterialPool.AstroInf.getGems(16),
-                VA_GregtechMaterialPool.AstroInf.getGems(16),
-                Materials.Redstone.getDust(64),
-                Materials.Redstone.getDust(64))
+                VA_GregtechMaterialPool.AstroInf.getGems(64),
+                VA_GregtechMaterialPool.AstroInf.getGems(64),
+                MaterialsUEVplus.Eternity.getNanite(64),
+                MaterialsUEVplus.Eternity.getNanite(64))
             .fluidInputs(
-                Materials.Water.getFluid(32000),
-                Materials.Lava.getFluid(32000),
-                Materials.Neutronium.getMolten(144000),
-                Materials.CosmicNeutronium.getMolten(144000),
-                Materials.Silver.getMolten(144000),
-                Materials.Infinity.getMolten(144000))
-            .itemOutputs(GT_OreDictUnificator.get(OrePrefixes.lens, VA_GregtechMaterialPool.AstroInf, 1))
+                MaterialsUEVplus.Universium.getMolten(144000000),
+                MaterialsUEVplus.ExcitedDTSC.getFluid(120000000),
+                Materials.Neutronium.getMolten(144000000),
+                Materials.CosmicNeutronium.getMolten(144000000),
+                MaterialsUEVplus.SpaceTime.getMolten(144000000),
+                Materials.Infinity.getMolten(144000000))
+            .itemOutputs(VAItemList.ItemUltimate.get(1))
             .noOptimize()
-            .eut(RECIPE_UXV)
-            .duration(900 * 20)
+            .eut(RECIPE_MAX)
+            .duration(16000 * 20)
             .addTo(AM);
     }
 }
