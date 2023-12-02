8c08d5098fdfc7500dfbef2c40fff5c84e4136b6
RhyVis <vssr@foxmail.com>
2023-12-02 09:21:48 +0800

    Add metaitem method

--- src/main/java/com/rhynia/gtnh/append/common/item/ItemRegister.java
+++ src/main/java/com/rhynia/gtnh/append/common/item/ItemRegister.java
@@ -7,6 +7,8 @@ import com.rhynia.gtnh.append.common.VAItemList;
 
 import cpw.mods.fml.common.registry.GameRegistry;
 
+import static com.rhynia.gtnh.append.common.item.registry.RegMetaItem01.initItem01;
+
 public class ItemRegister {
 
     public static void registryItems() {
@@ -20,6 +22,11 @@ public class ItemRegister {
 
     // spotless:off
     public static void registryItemContainers() {
+        // Meta Item
+        VAItemList.Test.set(initItem01("调试占位",0, new String[]{ "我相信它没什么用." }));
+        VAItemList.LapoMatrix.set(initItem01("兰波顿矩阵",1, new String[]{ "兰波顿密度达到了极致." }));
+        VAItemList.CrystalMatrix.set(initItem01("晶体矩阵",2, new String[]{ "用于批量生产各类晶体芯片." }));
+        // Special Item
         VAItemList.ItemUltimate.set(new ItemStack(ItemBasic.Ultimate,1));
         VAItemList.ItemLapoMatrix.set(new ItemStack(ItemBasic.LapoMatrix,1));
         VAItemList.ItemCrystalMatrix.set(new ItemStack(ItemBasic.CrystalMatrix,1));
