1c3d733eeb9473b1c793281b2f695bfbdf4a826b
RhyVis <vssr@foxmail.com>
2023-12-03 19:16:19 +0800

    rename tab name

--- src/main/java/com/rhynia/gtnh/append/common/block/BlockBasicItem.java
+++ src/main/java/com/rhynia/gtnh/append/common/block/BlockBasicItem.java
@@ -1,6 +1,6 @@
 package com.rhynia.gtnh.append.common.block;
 
-import static com.rhynia.gtnh.append.client.GTNHATab.tabMetaBlock01;
+import static com.rhynia.gtnh.append.client.VA_Tab.tabMetaBlock01;
 import static com.rhynia.gtnh.append.common.block.BlockBasic.MetaBlock01;
 import static com.rhynia.gtnh.append.util.UtilMetaItemStack.initMetaItemStack;
 import static com.rhynia.gtnh.append.util.UtilMetaItemStack.metaItemStackTooltipsAdd;
