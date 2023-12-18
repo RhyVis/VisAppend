7c20bcb25bb576ac5c833c9b7b12673a975e17ef
RhyVis <vssr@foxmail.com>
2023-12-18 20:31:18 +0800

    Quick fix for null input

--- src/main/java/com/rhynia/gtnh/append/common/recipePool/container/VARecipePool/VASSuperconductingBinderRecipePool.java
+++ src/main/java/com/rhynia/gtnh/append/common/recipePool/container/VARecipePool/VASSuperconductingBinderRecipePool.java
@@ -1,19 +1,16 @@
 package com.rhynia.gtnh.append.common.recipePool.container.VARecipePool;
 
-import static gregtech.api.enums.TierEU.RECIPE_MAX;
-import static gregtech.api.enums.TierEU.RECIPE_MV;
-import static gregtech.api.util.GT_RecipeBuilder.HOURS;
+import static gregtech.api.enums.TierEU.RECIPE_UMV;
 import static gregtech.api.util.GT_RecipeBuilder.INGOTS;
 import static gregtech.api.util.GT_RecipeBuilder.SECONDS;
 
 import com.rhynia.gtnh.append.api.recipe.AppendRecipeMaps;
 import com.rhynia.gtnh.append.common.VAItemList;
+import com.rhynia.gtnh.append.common.material.VAMaterials;
 import com.rhynia.gtnh.append.common.recipePool.IRecipePool;
 
 import gregtech.api.enums.GT_Values;
 import gregtech.api.enums.ItemList;
-import gregtech.api.enums.Materials;
-import gregtech.api.enums.MaterialsUEVplus;
 import gregtech.api.recipe.RecipeMap;
 import gregtech.api.recipe.RecipeMapBackend;
 
@@ -22,23 +19,7 @@ public class VASSuperconductingBinderRecipePool implements IRecipePool {
     private final RecipeMap<RecipeMapBackend> SB = AppendRecipeMaps.superconductingAssemblyRecipes;
 
     @Override
-    public void loadRecipesPostInit() {
-        // 终极配方-测试
-        GT_Values.RA.stdBuilder()
-            .itemInputs(MaterialsUEVplus.Eternity.getNanite(64), MaterialsUEVplus.Eternity.getNanite(64))
-            .fluidInputs(
-                MaterialsUEVplus.Universium.getMolten(1440000000),
-                MaterialsUEVplus.ExcitedDTSC.getFluid(1200000000),
-                Materials.CosmicNeutronium.getMolten(1440000000),
-                MaterialsUEVplus.Eternity.getMolten(1440000000),
-                MaterialsUEVplus.SpaceTime.getMolten(1440000000),
-                Materials.Infinity.getMolten(1440000000))
-            .itemOutputs(VAItemList.Test.get(1))
-            .noOptimize()
-            .eut(RECIPE_MAX)
-            .duration(160 * HOURS)
-            .addTo(SB);
-    }
+    public void loadRecipesPostInit() {}
 
     @Override
     public void loadRecipes() {
@@ -46,13 +27,11 @@ public class VASSuperconductingBinderRecipePool implements IRecipePool {
 
         // 终极配方-测试
         GT_Values.RA.stdBuilder()
-            .itemInputs(MaterialsUEVplus.BlackDwarfMatter.getNanite(64))
-            .fluidInputs(
-                Materials.Pentacadmiummagnesiumhexaoxid.getMolten(16 * INGOTS),
-                Materials.Helium.getMolten(16 * INGOTS))
-            .itemOutputs(ItemList.Superconducting_Magnet_Solenoid_MV.get(1))
+            .itemInputs(VAItemList.Test.get(1))
+            .fluidInputs(VAMaterials.SuperconductorFlux.getFluidOrGas(16 * INGOTS))
+            .itemOutputs(ItemList.Superconducting_Magnet_Solenoid_UMV.get(1))
             .noOptimize()
-            .eut(RECIPE_MV)
+            .eut(RECIPE_UMV)
             .duration(160 * SECONDS)
             .addTo(SB);
         // endregion
