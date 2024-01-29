e7daccc6eb01794c827ec8fd3b74221e679577c7
RhyVis <vssr@foxmail.com>
2024-01-29 12:08:43 +0800

    Remove air checks in all bw mega

---
+++ src/main/java/com/rhynia/gtnh/append/mixins/bw/MixinMegaMultiBase.java
@@ -0,0 +1,24 @@
+package com.rhynia.gtnh.append.mixins.bw;
+
+import net.minecraft.world.World;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@SuppressWarnings("SpellCheckingInspection")
+@Mixin(
+    targets = "com.github.bartimaeusnek.bartworks.common.tileentities.multis.mega.GT_TileEntity_MegaMultiBlockBase$StructureElementAirNoHint",
+    priority = 2000,
+    remap = false)
+public class MixinMegaMultiBase<T> {
+
+    /**
+     * This attempts to remove all air checks depending on StructureElementAirNoHint
+     */
+    @Inject(method = "check", at = @At("HEAD"), cancellable = true)
+    private void bh$injectAirCheck(T o, World world, int x, int y, int z, CallbackInfoReturnable<Boolean> cir) {
+        cir.setReturnValue(true);
+    }
+}
