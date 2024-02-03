fd1dd85f29fb0eb978ea8d66929c3a9aa8ef36d1
RhyVis <vssr@foxmail.com>
2024-02-03 18:19:27 +0800

    Tweak recipes

--- src/main/java/com/rhynia/gtnh/append/config/Config.java
+++ src/main/java/com/rhynia/gtnh/append/config/Config.java
@@ -19,7 +19,7 @@ public class Config {
     public static boolean Reccipe_TR_DTC_Enable = true;
     public static int Recipe_TR_RecipeMult = 32;
     public static int Recipe_TR_OutputMult = 250;
-    public static int Recipe_TR_CatalystACAMult = 16000;
+    public static int Recipe_TR_CatalystACAMult = 4000;
     public static int Recipe_TR_CatalystACRMult = 2000;
     // endregion
 
@@ -29,13 +29,14 @@ public class Config {
         Recipes_IA_Wireless_Enable = configuration
             .getBoolean("Recipes_IA_Wireless_Enable", RECIPE, true, "是否启用无线仓的IA简化配方");
         Reccipe_TR_DTC_Enable = configuration.getBoolean("Reccipe_TR_DTC_Enable", RECIPE, true, "是否启用TR超维度催化剂配方");
-        Recipe_TR_RecipeMult = configuration.getInt("Recipe_TR_RecipeMult", RECIPE, 32, 1, 64, "TR进行单次合成时的倍率");
+        Recipe_TR_RecipeMult = configuration
+            .getInt("Recipe_TR_RecipeMult", RECIPE, 128, 1, Integer.MAX_VALUE / 144 - 1, "TR进行单次合成时的倍率");
         Recipe_TR_OutputMult = configuration
             .getInt("Recipe_TR_OutputMult", RECIPE, 250, 1, Integer.MAX_VALUE / 144 - 1, "TR进行单次合成时输出催化剂的量，单位L");
         Recipe_TR_CatalystACAMult = configuration.getInt(
             "Recipe_TR_CatalystACAMult",
             RECIPE,
-            8000,
+            4000,
             16,
             Integer.MAX_VALUE / 16 - 1,
             "TR进行单次合成时的ACA催化剂消耗，不受合成倍率影响，单位L");
