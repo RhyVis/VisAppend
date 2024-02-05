4a5b36dd35ffeb5c8de8427867029c5aaecb936b
RhyVis <vssr@foxmail.com>
2024-02-05 10:23:16 +0800

    Add pre space recipe

--- src/main/java/com/rhynia/gtnh/append/common/recipe/GTRecipePool/VAPlasmaForgeRecipePool.java
+++ src/main/java/com/rhynia/gtnh/append/common/recipe/GTRecipePool/VAPlasmaForgeRecipePool.java
@@ -3,6 +3,7 @@ package com.rhynia.gtnh.append.common.recipe.GTRecipePool;
 import static com.rhynia.gtnh.append.api.enums.VA_Values.RecipeValues.RECIPE_UIV;
 import static com.rhynia.gtnh.append.api.enums.VA_Values.RecipeValues.RECIPE_UXV;
 import static com.rhynia.gtnh.append.api.enums.VA_Values.RecipeValues.SECONDS;
+import static gregtech.api.enums.Mods.GTPlusPlus;
 import static gregtech.api.util.GT_RecipeConstants.COIL_HEAT;
 
 import com.rhynia.gtnh.append.api.interfaces.IRecipePool;
@@ -10,9 +11,11 @@ import com.rhynia.gtnh.append.common.VAItemList;
 
 import gregtech.api.enums.GT_Values;
 import gregtech.api.enums.ItemList;
+import gregtech.api.enums.Materials;
 import gregtech.api.enums.MaterialsUEVplus;
 import gregtech.api.interfaces.IRecipeMap;
 import gregtech.api.recipe.RecipeMaps;
+import gregtech.api.util.GT_ModHandler;
 
 public class VAPlasmaForgeRecipePool implements IRecipePool {
 
@@ -35,6 +38,19 @@ public class VAPlasmaForgeRecipePool implements IRecipePool {
             .fluidInputs(MaterialsUEVplus.ExcitedDTSC.getFluid(1000))
             .fluidOutputs(MaterialsUEVplus.DimensionallyTranscendentResidue.getFluid(1000 * 2))
             .eut(RECIPE_UXV)
+            .duration(20 * SECONDS)
+            .metadata(COIL_HEAT, 10800 + 2700)
+            .addTo(PF);
+
+        GT_Values.RA.stdBuilder()
+            .itemInputs(
+                VAItemList.PreTesseract.get(4),
+                GT_ModHandler.getModItem(GTPlusPlus.ID, "MU-metaitem.01", 0, 32100))
+            .fluidInputs(
+                MaterialsUEVplus.DimensionallyTranscendentResidue.getFluid(20000),
+                Materials.Infinity.getMolten(4 * 1152))
+            .fluidOutputs(MaterialsUEVplus.SpaceTime.getMolten(4 * 576))
+            .eut(2_000_000_000)
             .duration(20 * SECONDS)
             .metadata(COIL_HEAT, 10800 + 2700)
             .addTo(PF);
